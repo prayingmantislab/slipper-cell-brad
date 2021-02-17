7d6900e41e4c234f930f3d382649e52b04c09524
Asaf Meir <office@elpuravida.co.il>
2021-02-17 20:45:14 +0200

    built the client's ui landing  and navbar

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Navbar from "./component/layout/Navbar";
+import Landing from "./component/layout/Landing";
+import "./App.css";
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Landing />
+  </Fragment>
+);
+
+export default App;
