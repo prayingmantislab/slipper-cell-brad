f0ad88ea313d8241c9fe7da621e2bbde032708d2
Asaf Meir <office@elpuravida.co.il>
2021-03-03 21:27:19 +0200

    added gravatar and jsonwebtokken to user

--- config/db.js
+++ config/db.js
@@ -4,8 +4,11 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(db
+      
+      , {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
       useCreateIndex: true
     });
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
-const { check, validationResult } = require("express-validator/check");
 
-const User = require("./models/User");
+// @route   post api/users
+// @desc    Register user
+// @access  Public
 
-// @route post api/users
-// @desc Register user
-// @access Public
 router.post(
   "/",
   [
@@ -26,14 +26,13 @@ router.post(
     }
 
     const { name, email, password } = req.body;
+
     try {
       let user = await User.findOne({ email });
-      if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
-      }
 
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
       const avatar = gravatar.url(email, {
         s: "200",
         r: "pg",
@@ -50,11 +49,11 @@ router.post(
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
+      await user.save();
 
-      await user
-        .save()
-        // Return jsonwebtoken
-        .res.send("User registered");
+      // Return jsonwebtoken
+
+      res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
