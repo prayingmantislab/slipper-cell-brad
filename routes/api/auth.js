4d4642fe58414b13a339157b4ca3ee4336da9d4c
Asaf Meir <office@elpuravida.co.il>
2021-03-14 22:19:06 +0200

    built register component

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const auth = require('../../middleware/auth');
+const auth = require('./../../config/middleware/auth');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
@@ -11,7 +11,7 @@ const User = require('../../models/User');
 // @route Get api/auth
 // @desc Test route
 // @access Public
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
     res.json(user);
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,24 +1,24 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-const User = require("../../models/User");
+const User = require('../../models/User');
 // @route   post api/users
 // @desc    Register user
 // @access  Public
 
 router.post(
-  "/",
+  '/',
 
-  check("name", "Name is required").notEmpty(),
-  check("email", "Please include a valid email").isEmail(),
+  check('name', 'Name is required').notEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
   check(
-    "password",
-    "Please enter a password with 6 or more characters"
+    'password',
+    'Please enter a password with 6 or more characters'
   ).isLength({ min: 6 }),
   async (req, res) => {
     const errors = validationResult(req);
@@ -34,13 +34,13 @@ router.post(
       if (user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: "User already exists" }] });
+          .json({ errors: [{ msg: 'User already exists' }] });
       }
 
       const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
+        s: '200',
+        r: 'pg',
+        d: 'mm',
       });
 
       user = new User({
@@ -62,14 +62,18 @@ router.post(
         },
       };
 
-      jwt.sign(payload, config.get("jwtSecret"), { expiresIn: 360000 });
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Server error");
+      res.status(500).send('Server error');
     }
   }
 );
