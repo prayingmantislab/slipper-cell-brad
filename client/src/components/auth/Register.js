32d2dbc0b870fc0680bb7d8dc4de1621b687ce74
Asaf Meir <office@elpuravida.co.il>
2021-03-24 23:01:10 +0200

    added set alert and remove alert action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,8 +2,9 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -14,14 +15,16 @@ const Register = (props) => {
   const { name, email, password, password2 } = formData;
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert('Password do not match', 'danger');
+      setAlert('Password do not match', 'danger');
     } else {
       console.log('Success');
     }
   };
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
@@ -80,6 +83,10 @@ const Register = (props) => {
       </p>
     </Fragment>
   );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
 };
 
 export default connect(null, { setAlert })(Register);
