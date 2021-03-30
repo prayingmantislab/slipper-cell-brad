40f2ebb118172f3d2a7aee4cab26e6463d2117dc
Asaf Meir <office@elpuravida.co.il>
2021-03-30 19:25:44 +0300

    in order to merge master

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
-
+//added this line jus to add a commit
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
