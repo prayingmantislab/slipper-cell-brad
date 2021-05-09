45e8be91bbd38c6f6ac94a74d51023a51e1a4552
Asi Meir <office@elpuravida.co.il>
2021-05-09 12:28:38 +0300

    added stat model and stats api

---
+++ models/Stat.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StatSchema = new Schema({
+  id: {},
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  light: {
+    type: Number,
+  },
+  noise: {
+    type: Number,
+  },
+  sleepTime: {
+    type: Date,
+  },
+  wakeTime: {
+    type: Date,
+  },
+  totalSleep: {
+    type: Number,
+  },
+  sleepScore: {
+    type: Number,
+  },
+});
+
+module.exports = Stat = mongoose.model('stat', StatSchema);
